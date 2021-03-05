052137155aba7b076ff9b815d2c6d4b1fa7dbc41
coderootme <34464627+coderootme@users.noreply.github.com>
2021-03-05 09:57:02 +0100

    Create raspbian-liter.sh

---
+++ raspbian-liter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# this makes raspbian lite even liter
+
+sudo su
+systemctl disable dphys-swapfile.service avahi-daemon.service rpi-eeprom-update.service
+systemctl disable apt-daily-upgrade.timer apt-daily.timer man-db.timer
+swapoff -a
+rm /var/swap
+apt update
+apt -y purge bluez bluez-firmware
+apt -y install vim tmux mc
+mkdir -p /tmpfs/
+{
+   echo ''
+   echo 'tmpfs /tmpfs/ tmpfs nodev,nosuid,size=512M   0 0'
+} >> /etc/fstab
+mount -a
+{
+   echo ''
+   echo 'dtoverlay=disable-bt'
+} >> /boot/config.txt
